1dbb2b48baa3b3e62cb0c0f11072dcd181a76211
SDNATH-Git <sd.nath1952@gmail.com>
2025-10-23 01:14:07 +0600

    addedd loader

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,44 @@ import router from "./Routers/Router";
 import AuthProvider from "./Provider/AuthProvider";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Loder from "./components/Loder";
+
+//Loader contion
+function App() {
+  const [isLoading, setIsLoading] = useState(true);  // ✅ state এর নাম আলাদা
+
+  useEffect(() => {
+    const timer = setTimeout(() => setIsLoading(false), 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <AnimatePresence mode="wait">
+      {isLoading ? (
+        <motion.div
+          key="loader"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
+          <Loder />
+        </motion.div>
+      ) : (
+        <motion.div
+          key="app"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.8 }}
+        >
+          <RouterProvider router={router} />
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+}
+
+//Loader contion
 
 const queryClient = new QueryClient();
 
